3bead1808f1c4e37a0490f92ffc9e2d0b8e066b5
Janakiram MSV <janakiramm@users.noreply.github.com>
2016-07-09 23:00:24 +0530

    Added kubctl download command

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -2,5 +2,9 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.5.0/miniku
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
 
+curl -Lo kubectl http://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/darwin/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
 minikube start
 kubectl config use-context minikube
